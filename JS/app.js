886a2bb9761bbe3d65b71f89b9ae319f08d02d75
JongoSimon <simonjongo@gmail.com>
2025-04-17 10:13:13 -0500

    conditional statement updated

--- JS/app.js
+++ JS/app.js
@@ -49,3 +49,25 @@ numThree += 2;
 console.log(numThree);
 numThree *= 3;
 console.log(numThree);
+
+//! Day 4 Combining variable, data types and Operations
+
+//* Calculating Total Price with Discount
+
+let originalPrice = 100;
+let discountRate = 0.2;
+let isMember = true;
+
+//console.log(originalPrice)
+
+let discountAmount = originalPrice * discountRate;
+console.log(discountAmount);
+let discountedPrice = originalPrice - discountAmount;
+console.log(discountedPrice)
+
+//* Additional Discount for Members
+if (isMember) {
+    discountedPrice -=10;
+}
+
+console.log("Discounted Price:", discountedPrice)
